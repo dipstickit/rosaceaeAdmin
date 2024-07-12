e4bfab320177ffbb92192f087176fd7e35d70974
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-07-13 01:17:58 +0700

    fix handleUI

--- src/content/dashboards/Crypto/index.tsx
+++ src/content/dashboards/Crypto/index.tsx
@@ -9,10 +9,14 @@ import WatchList from './WatchList';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import OrderDetailService, { DailyOrderCountResponse, DailyPriceForAdminResponse, DailyPriceForShopResponse } from 'src/api/OrderDetail.service';
+import OrderDetailService, {
+  DailyOrderCountResponse,
+  DailyPriceForAdminResponse,
+  DailyPriceForShopResponse
+} from 'src/api/OrderDetail.service';
 import { setUser } from 'src/redux/slices/auth.slice';
 import { useAppDispatch } from 'src/redux/store';
-import jwt_decode from "jwt-decode";
+import jwt_decode from 'jwt-decode';
 import UserService from '../../../api/User.services';
 import BookingService from 'src/api/Booking.service';
 
@@ -24,89 +28,150 @@ export interface ChartSeries {
 function DashboardCrypto() {
   let navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const today = new Date()
-  const colorArr = ['#b2b5b2', '#8291ff', '#57ca22', '#e74c3c']
+  const today = new Date();
+  const colorArr = ['#b2b5b2', '#8291ff', '#57ca22', '#e74c3c'];
   const [chartSeries, setChartSeries] = useState<ChartSeries[]>([]);
-  const [percentage, setPercentage] = useState<number[]>([])
-  const [statusLabel, setStatusLabel] = useState<string[]>([])
-  const [revenueDayList, setRevenueDayList] = useState<DailyPriceForShopResponse[]>()
-  const [dayList, setDayList] = useState<string[]>()
-  const [revenue, setRevenue] = useState<number[]>()
-  const [orderList, setOrderList] = useState<number[]>()
-  const [accountBalance, setAccountBalance] = useState<number>(0)
-  const [monthArr, setMonthArr] = useState<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-  const [yearArr, setYearArr] = useState<number[]>(Array.from({ length: today.getFullYear() - 2000 + 1 }, (_, i) => today.getFullYear() - i).slice().reverse());
-  const [month, setMonth] = useState<any>((today.getMonth() + 1))
-  const [year, setYear] = useState<any>(today.getFullYear())
-  const [totalOrder, setTotalOrder] = useState<number>(0)
-  let accessToken: string = useSelector((state: any) => state.auth.userToken) !== null ?
-    useSelector((state: any) => state.auth.userToken) : localStorage.getItem("userToken")
-  let [userInfo, setUserInfo] = useState<any>(useSelector((state: any) => state.auth.userInfo))
+  const [percentage, setPercentage] = useState<number[]>([]);
+  const [statusLabel, setStatusLabel] = useState<string[]>([]);
+  const [revenueDayList, setRevenueDayList] =
+    useState<DailyPriceForShopResponse[]>();
+  const [dayList, setDayList] = useState<string[]>();
+  const [revenue, setRevenue] = useState<number[]>();
+  const [orderList, setOrderList] = useState<number[]>();
+  const [accountBalance, setAccountBalance] = useState<number>(0);
+  const [monthArr, setMonthArr] = useState<number[]>([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+  ]);
+  const [yearArr, setYearArr] = useState<number[]>(
+    Array.from(
+      { length: today.getFullYear() - 2000 + 1 },
+      (_, i) => today.getFullYear() - i
+    )
+      .slice()
+      .reverse()
+  );
+  const [month, setMonth] = useState<any>(today.getMonth() + 1);
+  const [year, setYear] = useState<any>(today.getFullYear());
+  const [totalOrder, setTotalOrder] = useState<number>(0);
+  let accessToken: string =
+    useSelector((state: any) => state.auth.userToken) !== null
+      ? useSelector((state: any) => state.auth.userToken)
+      : localStorage.getItem('userToken');
+  let [userInfo, setUserInfo] = useState<any>(
+    useSelector((state: any) => state.auth.userInfo)
+  );
 
   const getUserByEmail = async () => {
     var decoded = jwt_decode(accessToken);
-    console.log(decoded)
-    const response = await UserService.getUserByEmail(decoded["sub"], accessToken)
+    console.log(decoded);
+    const response = await UserService.getUserByEmail(
+      decoded['sub'],
+      accessToken
+    );
     if (response.status === 403 || response.status === 401) {
       localStorage.removeItem('userToken');
-      navigate('/login')
-      return
+      navigate('/login');
+      return;
     }
-    setUserInfo(response.data['userInfo'])
-    console.log(userInfo)
+    setUserInfo(response.data['userInfo']);
+    console.log(userInfo);
     dispatch(setUser(userInfo));
-  }
+  };
 
   if (userInfo === null) {
-    console.log("user is null")
-    getUserByEmail()
+    console.log('user is null');
+    getUserByEmail();
   }
   const fetchData = async () => {
     try {
-      let response = null
-      let response_1 = null
-      let response_2 = null
-      let response_3 = null
-      response = userInfo.role === 'ADMIN' ?
-        await OrderDetailService.getRevenueEachDayAdmin(today.getMonth() + 1, today.getFullYear(), accessToken)
-        :
-        await OrderDetailService.getRevenueEachDay(userInfo.usersID, today.getMonth() + 1, today.getFullYear(), accessToken);
-      response_1 = userInfo.role === 'ADMIN' ?
-        await OrderDetailService.getTotalPriceAdmin(today.getMonth() + 1, today.getFullYear(), accessToken)
-        :
-        await OrderDetailService.getTotalPrice(userInfo.usersID, today.getMonth() + 1, today.getFullYear(), accessToken);
-      response_2 = userInfo.role === 'ADMIN' ?
-        await BookingService.getOrderStatusPercentageAdmin(accessToken)
-        :
-        await BookingService.getOrderStatusPercentage(userInfo.usersID, accessToken);
-      response_3 = userInfo.role === 'ADMIN' ?
-        await OrderDetailService.getOrderEachDayAdmin(today.getMonth() + 1, today.getFullYear(), accessToken)
-        :
-        await OrderDetailService.getOrderEachDay(userInfo.usersID, today.getMonth() + 1, today.getFullYear(), accessToken)
+      let response = null;
+      let response_1 = null;
+      let response_2 = null;
+      let response_3 = null;
+      response =
+        userInfo.role === 'ADMIN'
+          ? await OrderDetailService.getRevenueEachDayAdmin(
+              today.getMonth() + 1,
+              today.getFullYear(),
+              accessToken
+            )
+          : await OrderDetailService.getRevenueEachDay(
+              userInfo.usersID,
+              today.getMonth() + 1,
+              today.getFullYear(),
+              accessToken
+            );
+      response_1 =
+        userInfo.role === 'ADMIN'
+          ? await OrderDetailService.getTotalPriceAdmin(
+              today.getMonth() + 1,
+              today.getFullYear(),
+              accessToken
+            )
+          : await OrderDetailService.getTotalPrice(
+              userInfo.usersID,
+              today.getMonth() + 1,
+              today.getFullYear(),
+              accessToken
+            );
+      response_2 =
+        userInfo.role === 'ADMIN'
+          ? await BookingService.getOrderStatusPercentageAdmin(accessToken)
+          : await BookingService.getOrderStatusPercentage(
+              userInfo.usersID,
+              accessToken
+            );
+      response_3 =
+        userInfo.role === 'ADMIN'
+          ? await OrderDetailService.getOrderEachDayAdmin(
+              today.getMonth() + 1,
+              today.getFullYear(),
+              accessToken
+            )
+          : await OrderDetailService.getOrderEachDay(
+              userInfo.usersID,
+              today.getMonth() + 1,
+              today.getFullYear(),
+              accessToken
+            );
       console.log('Response data:', response.data);
       console.log('Response_1 data:', response_1.data);
       console.log('Response_2 data:', response_2.data);
       console.log('Response_3 data:', response_3.data);
-      const transformedData: any = Object.entries(response_2.data).map(([name, value]) => ({
-        name,
-        value,
-      }));
-      const dayList = response.data.map((item: DailyPriceForShopResponse) => `Day ${item.day.toString()}`)
-      const revenueList = userInfo.role === 'ADMIN' ?
-        response.data.map((item: DailyPriceForAdminResponse) => item.totalPriceForAdmin)
-        :
-        response.data.map((item: DailyPriceForShopResponse) => item.totalPriceForShop)
-      const orderList = response_3.data.map((item: DailyOrderCountResponse) => item.count)
-      const totalOrder = orderList.reduce((acc, num) => acc + num, 0)
-      setChartSeries(transformedData)
-      setPercentage(Object.values(response_2.data))
-      setStatusLabel(Object.keys(response_2.data))
-      setRevenueDayList(response.data)
-      setDayList(dayList)
-      setRevenue(revenueList)
-      setOrderList(orderList)
-      setAccountBalance(userInfo.role === 'ADMIN' ? response_1.data['totalPriceForAdmin'] : response_1.data['totalPriceForShop'])
-      setTotalOrder(totalOrder)
+      const transformedData: any = Object.entries(response_2.data).map(
+        ([name, value]) => ({
+          name,
+          value
+        })
+      );
+      const dayList = response.data.map(
+        (item: DailyPriceForShopResponse) => `Day ${item.day.toString()}`
+      );
+      const revenueList =
+        userInfo.role === 'ADMIN'
+          ? response.data.map(
+              (item: DailyPriceForAdminResponse) => item.totalPriceForAdmin
+            )
+          : response.data.map(
+              (item: DailyPriceForShopResponse) => item.totalPriceForShop
+            );
+      const orderList = response_3.data.map(
+        (item: DailyOrderCountResponse) => item.count
+      );
+      const totalOrder = orderList.reduce((acc, num) => acc + num, 0);
+      setChartSeries(transformedData);
+      setPercentage(Object.values(response_2.data));
+      setStatusLabel(Object.keys(response_2.data));
+      setRevenueDayList(response.data);
+      setDayList(dayList);
+      setRevenue(revenueList);
+      setOrderList(orderList);
+      setAccountBalance(
+        userInfo.role === 'ADMIN'
+          ? response_1.data['totalPriceForAdmin']
+          : response_1.data['totalPriceForShop']
+      );
+      setTotalOrder(totalOrder);
     } catch (error) {
       console.error('Error fetching items:', (error as Error).message);
     }
@@ -119,62 +184,102 @@ function DashboardCrypto() {
   }, [userInfo]);
 
   const fetchAccountBalance = async () => {
-    console.log(`${month} - ${year}`)
-    const res = await OrderDetailService.getTotalPrice(userInfo.usersID, month, year, accessToken);
-    console.log(res.data)
-    setAccountBalance(res.data.totalPriceForShop)
-  }
+    console.log(`${month} - ${year}`);
+    const res = await OrderDetailService.getTotalPrice(
+      userInfo.usersID,
+      month,
+      year,
+      accessToken
+    );
+    console.log(res.data);
+    setAccountBalance(res.data.totalPriceForShop);
+  };
 
   const fetchChart = async () => {
-    console.log(`${month} - ${year}`)
+    console.log(`${month} - ${year}`);
     try {
-      let response = null
-      let response_1 = null
-      let response_2 = null
-      fetchAccountBalance()
-      response = userInfo.role === 'ADMIN' ?
-        await OrderDetailService.getRevenueEachDayAdmin(month, year, accessToken)
-        :
-        await OrderDetailService.getRevenueEachDay(userInfo.usersID, month, year, accessToken);
-      response_1 = userInfo.role === 'ADMIN' ?
-        await OrderDetailService.getTotalPriceAdmin(month, year, accessToken)
-        :
-        await OrderDetailService.getTotalPrice(userInfo.usersID, month, year, accessToken);
-      response_2 = userInfo.role === 'ADMIN' ?
-        await OrderDetailService.getOrderEachDayAdmin(month, year, accessToken)
-        :
-        await OrderDetailService.getOrderEachDay(userInfo.usersID, month, year, accessToken)
+      let response = null;
+      let response_1 = null;
+      let response_2 = null;
+      fetchAccountBalance();
+      response =
+        userInfo.role === 'ADMIN'
+          ? await OrderDetailService.getRevenueEachDayAdmin(
+              month,
+              year,
+              accessToken
+            )
+          : await OrderDetailService.getRevenueEachDay(
+              userInfo.usersID,
+              month,
+              year,
+              accessToken
+            );
+      response_1 =
+        userInfo.role === 'ADMIN'
+          ? await OrderDetailService.getTotalPriceAdmin(
+              month,
+              year,
+              accessToken
+            )
+          : await OrderDetailService.getTotalPrice(
+              userInfo.usersID,
+              month,
+              year,
+              accessToken
+            );
+      response_2 =
+        userInfo.role === 'ADMIN'
+          ? await OrderDetailService.getOrderEachDayAdmin(
+              month,
+              year,
+              accessToken
+            )
+          : await OrderDetailService.getOrderEachDay(
+              userInfo.usersID,
+              month,
+              year,
+              accessToken
+            );
       console.log('Response headers:', response.headers);
       console.log('Response data:', response.data);
-      setRevenueDayList(response.data)
-      const dayList = response.data.map((item: DailyPriceForShopResponse) => `Day ${item.day.toString()}`)
-      const revenueList = userInfo.role === 'ADMIN' ?
-        response.data.map((item: DailyPriceForAdminResponse) => item.totalPriceForAdmin)
-        :
-        response.data.map((item: DailyPriceForShopResponse) => item.totalPriceForShop)
-      const orderList = response_2.data.map((item: DailyOrderCountResponse) => item.count)
-      const totalOrder = orderList.reduce((acc, num) => acc + num, 0)
-      setDayList(dayList)
-      setRevenue(revenueList)
-      setTotalOrder(totalOrder)
-      setOrderList(orderList)
+      setRevenueDayList(response.data);
+      const dayList = response.data.map(
+        (item: DailyPriceForShopResponse) => `Day ${item.day.toString()}`
+      );
+      const revenueList =
+        userInfo.role === 'ADMIN'
+          ? response.data.map(
+              (item: DailyPriceForAdminResponse) => item.totalPriceForAdmin
+            )
+          : response.data.map(
+              (item: DailyPriceForShopResponse) => item.totalPriceForShop
+            );
+      const orderList = response_2.data.map(
+        (item: DailyOrderCountResponse) => item.count
+      );
+      const totalOrder = orderList.reduce((acc, num) => acc + num, 0);
+      setDayList(dayList);
+      setRevenue(revenueList);
+      setTotalOrder(totalOrder);
+      setOrderList(orderList);
     } catch (error) {
       console.error('Error fetching items:', (error as Error).message);
     }
-  }
+  };
 
   const calculateTotalNumberOfOrder = () => {
     let total: number = 0;
     for (let i = 0; i < orderList.length; i++) {
-      total += orderList[i]
+      total += orderList[i];
     }
-    setTotalOrder(total)
-  }
+    setTotalOrder(total);
+  };
 
   return (
     <>
       <Helmet>
-        <title>Crypto Dashboard</title>
+        <title>Admin Rosaceae</title>
       </Helmet>
       <PageTitleWrapper>
         <PageHeader
--- src/redux/actions/auth.actions.ts
+++ src/redux/actions/auth.actions.ts
@@ -6,43 +6,50 @@ import { LoginArgs } from '../types/auth.types';
 import { HttpStatusCode } from 'axios';
 import toast from 'react-hot-toast';
 
-export const loginAPI = createAsyncThunk('auth/login', async ({ email, password }: LoginArgs, thunkAPI) => {
-  try {
-    const response = await instance.post<ResponseSuccessful<LoginResponse>>('auth/login', {
-      email,
-      password
-    });
-    console.log('Login API Response:', response.data);
+export const loginAPI = createAsyncThunk(
+  'auth/login',
+  async ({ email, password }: LoginArgs, thunkAPI) => {
+    try {
+      const response = await instance.post<ResponseSuccessful<LoginResponse>>(
+        'auth/login',
+        {
+          email,
+          password
+        }
+      );
+      console.log('Login API Response:', response.data);
 
-    if (response.status === HttpStatusCode.Unauthorized) {
-      toast.error("Unauthorized: Incorrect credentials");
-      return;
-    }
-    if (!response.data) {
-      toast.error("Access token not found in response");
-      return;
-    
-    }
-    const { access_token, userInfo } = response.data;
-    return { access_token, userInfo };
-  } catch (error: any) {
-    console.log("Login API Error:", error);
-    if (error.response && error.response.data.error_message) {
-      return thunkAPI.rejectWithValue(error.response.data.error_message);
-    } else {
-      return thunkAPI.rejectWithValue(error.message);
+      if (response.status === HttpStatusCode.Unauthorized) {
+        toast.error('Unauthorized: Incorrect credentials');
+        return;
+      }
+      if (!response.data) {
+        toast.error('Access token not found in response');
+        return;
+      }
+      const { access_token, userInfo } = response.data;
+      return { access_token, userInfo };
+    } catch (error: any) {
+      console.log('Login API Error:', error);
+      if (error.response && error.response.data.error_message) {
+        return thunkAPI.rejectWithValue(error.response.data.error_message);
+      } else {
+        return thunkAPI.rejectWithValue(error.message);
+      }
     }
   }
-});
+);
 
-
-export const logoutAPI = createAsyncThunk('auth/logout', async (token: string, thunkAPI) => {
-  try {
-    const response = await instance.post(`/auth/logout?token=${token}`);
-    localStorage.removeItem('user');
-    localStorage.removeItem('token');
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error);
+export const logoutAPI = createAsyncThunk(
+  'auth/logout',
+  async (token: string, thunkAPI) => {
+    try {
+      const response = await instance.post(`/auth/logout?token=${token}`);
+      localStorage.removeItem('user');
+      localStorage.removeItem('token');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
   }
-});
+);
